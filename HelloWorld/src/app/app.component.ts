9e1793f91d282a64a42a18f8404c62da0f9f1ad5
abhi5926 <abhisheksen5926@gmail.com>
2025-03-08 14:58:39 +0530

    [Abhishek] Add. Display Hello from BridgeLabz with a user inputted name

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -13,6 +13,7 @@ export class AppComponent {
   imgUrl ="assets/logo.jpg";
   url = "https://www.bridgelabz.com";
  userName = "";
+ nameError = "";
  
   ngOnInit(): void {
     this.title = "Hello from BridgeLabz.";
@@ -22,4 +23,15 @@ export class AppComponent {
     console.log("Save button is clicked ", $event);
     window.open(this.url, "_blenk");
   }
+
+  onInput($event: Event) {
+    console.log("Change Event Occurred!", $event);
+    const nameRegex = /^[A-Z][a-zA-Z\s]{2,}$/; 
+    if (nameRegex.test(this.userName)) {
+      this.nameError = "";
+    } else {
+      this.nameError = "Name is Incorrect!";
+    }
+  }
+
 }
