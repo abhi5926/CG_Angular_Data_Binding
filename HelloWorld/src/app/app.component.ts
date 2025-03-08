3b77cf599672a1aac33a961fddb21c6e87314133
abhi5926 <abhisheksen5926@gmail.com>
2025-03-08 06:14:07 +0530

    [Abhishek] Add. [Abhishek] Add. To Display BridgeLabz Logo using Property Binding Technique

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HelloWorld';
+  imgUrl ="assets/logo.jpg";
 
   ngOnInit(): void {
     this.title = "Hello from BridgeLabz.";
